0f99fc73a442c23ebe228a2201c9460cb96d2f14
19sandip <sandeep307098@gmail.com>
2025-07-26 16:18:03 +0530

    added backend url after deployment

--- frontend/src/authentication/Authentication.js
+++ frontend/src/authentication/Authentication.js
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext({});
 
-const server = "todo-dd5tl1q9h-sandeep-kumars-projects-9189288a.vercel.app"
+const server = "https://todo-app-wt7k.onrender.com"
 
 const client = axios.create({
   baseURL: `${server}/api`,
